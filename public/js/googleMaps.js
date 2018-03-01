b96d955422b0da3722bb115c6ebd625598189098
Tiffany Trinh <tttrinh@ucsd.edu>
2018-03-01 02:12:00 -0800

    lab 8

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
-}
+	var ucsd_ltlng = {lat:32.878911, lng:-117.235901};
+	
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+	}
+
